af90c3815ce40cfeeaf98dd898b6098fe5f13640
wdwiii <wdwhitfield3@gmail.com>
2022-06-13 09:10:39 -0400

    Create get and post methods for application

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => res.send('You can pot to this endpoint'));
+app.post(`/api/v1/tours`, (req, res) => {
+  const newId = indexOf(tours[tours.length - 1]) + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
