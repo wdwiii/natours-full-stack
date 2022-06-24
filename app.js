6b2544f4f957eec1360572084a7ab8f8707e7272
wdwiii <wdwhitfield3@gmail.com>
2022-06-23 20:20:57 -0400

    Set up environment variables
    - Use express.static for public folder
    - Only use mogan in dev environment
    - assign port var in server.js to the PORT env variable

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ const userRouter = require('./routes/userRoutes');
 //MIDDLEWARE
 //=====================
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: `./config.env` }); //Needs to be required before it reads the app file
+
 const app = require('./app');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //=====================
 //LISTENER
