79421f931f599ad9134a43300b73768bed9ddb5c
wdwiii <wdwhitfield3@gmail.com>
2022-06-28 22:43:12 -0400

    Add line after require statements

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: `./config.env` }); //Needs to be required before it reads the app file
 
 const app = require('./app');
+
 const port = process.env.PORT || 3000;
 
 //=====================
