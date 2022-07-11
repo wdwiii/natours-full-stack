1f588d9931dcd91070e0bdd6e7ba63b6e4412b02
wdwiii <wdwhitfield3@gmail.com>
2022-07-10 23:49:16 -0400

    Create tours schema

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: `./config.env` }); //Needs to be required before it reads the app file
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+  });
+
 const port = process.env.PORT || 3000;
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A name is required'],
+    unique: true,
+  },
+  rating: { type: Number, required: false, default: 4.5 },
+  price: { type: Number, required: [true, 'A price is required'] },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
 
 //=====================
 //LISTENER
