3a1988d4eed1c6de9c335c95f7dccbbe3203016c
wdwiii <wdwhitfield3@gmail.com>
2022-07-20 13:48:02 -0400

    Implement pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,6 +39,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //Pagination
+    //page=3&limit=3
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     //Execute Query
     const tours = await query;
 
@@ -53,6 +68,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
+      message: err.message,
     });
   }
 };
